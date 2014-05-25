2ff02b9779bd5080cf718e922c1023e8e25b40ff
unknown <user@user-PC.(none)>
2014-05-26 07:46:37 +1000

    Doing parameter search on Higgs example

--- utils/peter_common.h
+++ utils/peter_common.h
@@ -12,7 +12,11 @@ struct Logger {
     void write_log(const char *file, int line, const char *format, ...);
 };
 
+#ifdef PETER_DEBUG
 extern Logger *logger;
 #define log_debug(...) logger->write_log(__FILE__, __LINE__, __VA_ARGS__)
+#else
+#define log_debug(...)
+#endif 
 
 #endif // #ifndef PETER_COMMON
